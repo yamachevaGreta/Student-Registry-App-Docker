6f43ca39fe78ff180eac5f0988f069bebead1e76
Greta Yamacheva <yamacheva@gmail.com>
2025-02-16 17:18:41 +0200

    delete dummy comment:

--- tests/add-student.tests.js
+++ tests/add-student.tests.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
-//test comment again
 suite('Add Students page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/add-student");
