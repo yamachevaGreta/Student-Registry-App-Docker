4c388ba7d7756c9bfbd9e87ffdbd8ef817ca6dee
Greta Yamacheva <yamacheva@gmail.com>
2025-02-16 15:59:13 +0200

    change comment

--- tests/add-student.tests.js
+++ tests/add-student.tests.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const fetch = require('node-fetch');
 
-//test comment
+//test comment again
 suite('Add Students page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/add-student");
