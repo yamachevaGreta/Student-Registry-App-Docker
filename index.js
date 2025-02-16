26dc0010ed832335ceb470e06f589ee86d7877cf
Greta Yamacheva <yamacheva@gmail.com>
2025-02-16 21:17:23 +0200

    change port

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ studentsController.setup(app, students);
 
 let port = process.argv[2];
 if (!port) port = process.env['PORT'];
-if (!port) port = 8081;
+if (!port) port = 3030;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
   console.log(`App started. Listening at http://localhost:${port}`);
 })
 .on('error', function(err) {
